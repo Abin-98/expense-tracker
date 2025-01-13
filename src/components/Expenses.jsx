415d70bad3196bf2d3a718ec184416856e1bc8b7
Abin-98 <abinvarkey01998@gmail.com>
2025-01-13 16:32:05 +0530

    realtime db used to store expense list

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,22 +1,56 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ContextFile } from "../context/ContextFile";
-import List from "./List"
+import List from "./List";
+import axios from "axios";
 
 const Expenses = () => {
   const { expenseList, setExpenseList } = useContext(ContextFile);
   const [expense, setExpense] = useState({
-    id: '',
+    id: "",
     amount: 0,
     description: "",
-    category: "",
+    category: "Food",
   });
+  const assetList = ["Assets-Profit", "Savings", "Salary"];
+
+  const netGain=Number(expenseList?.filter((item) =>
+        assetList?.some((asset) => asset === item.category)
+      )
+      .reduce((acc, item) => acc + Number(item.amount), 0)
+    ).toFixed(2)
+
+  const netLoss=Number(expenseList?.filter((item) =>
+          !assetList?.some((asset) => asset === item.category)
+      )
+      .reduce((acc, item) => acc + Number(item.amount), 0)  // Number() because data from realtime db is string
+      ).toFixed(2)
+
+  useEffect(() => {
+    axios
+      .get("https://expense-tracker-da8bb-default-rtdb.firebaseio.com/expenses.json")
+      .then((res) => {
+
+        setExpenseList(Object.values(res.data))
+        console.log(Object.values(res.data))
+        })
+      .catch((err) => console.log(err));
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     expense.id = Math.floor(Math.random() * 10000);
     setExpenseList([...expenseList, expense]);
-    console.log(expenseList);
-    
+    axios
+      .post(
+        "https://expense-tracker-da8bb-default-rtdb.firebaseio.com/expenses.json",
+        {
+          description: expense.description,
+          amount: expense.amount,
+          category: expense.category,
+        }
+      )
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   };
   return (
     <section className="grid grid-cols-5 gap-5">
@@ -25,9 +59,7 @@ const Expenses = () => {
           <h1 className="text-2xl pb-4 font-semibold">Add new transaction</h1>
           <hr className="w-[20rem] border-blue-600" />
           <form className="flex flex-col mt-7" onSubmit={handleSubmit}>
-            <h1 className="text-lg mb-2">
-              Amount spent - negative for expense, positive for income
-            </h1>
+            <h1 className="text-lg mb-2">Amount spent / earned</h1>
             <input
               value={expense.amount}
               className="p-2 px-3 border-[1px] rounded-md mb-6"
@@ -49,32 +81,34 @@ const Expenses = () => {
               required
             />
             <h1 className="text-lg mb-2">Select Category</h1>
-            <input
+            <select
               value={expense.category}
               className="p-2 px-3 border-[1px] rounded-md mb-6"
-              list="browsers"
-              name="browser"
               onChange={(e) =>
                 setExpense({ ...expense, category: e.target.value })
               }
-              placeholder="Double-click to see dropdown"
+              placeholder="Food"
               required
-            />
-            <datalist id="browsers">
-              <option value="Food" />
-              <option value="Petrol" />
-              <option value="Salary" />
-              <option value="Entertainment" />
-              <option value="Travel" />
-              <option value="Education" />
-              <option value="Rent" />
-              <option value="Clothes" />
-              <option value="Home utensils" />
-              <option value="Hospital bills" />
-              <option value="Donation" />
-              <option value="Others" />
-              <option value="Investment" />
-            </datalist>
+            >
+              <option value="Food">Food</option>
+              <option value="Petrol">Petrol</option>
+              <option value="Entertainment">Entertainment</option>
+              <option value="Travel">Travel</option>
+              <option value="Education">Education</option>
+              <option value="Rent">Rent</option>
+              <option value="Clothes">Clothes</option>
+              <option value="Furniture">Furniture</option>
+              <option value="Hospital-bills">Hospital-bills</option>
+              <option value="Donation">Donation</option>
+              <option value="Investment">Investment</option>
+              <option value="Groceries">Groceries</option>
+              <option value="Kitchen-utensils">Kitchen-utensils</option>
+              <option value="Maintenance">Maintenance</option>
+              <option value="Repair">Repair</option>
+              <option value="Assets-Profit">Assets-Profit</option>
+              <option value="Savings">Savings</option>
+              <option value="Salary">Salary</option>
+            </select>
             <div className="flex justify-center">
               <button
                 className="w-[10rem] p-2 bg-blue-800 text-white text-lg mt-6"
@@ -88,26 +122,35 @@ const Expenses = () => {
       </div>
       <div className="flex justify-center col-span-3">
         <div className="flex flex-col mt-3 min-w-[20rem] w-[30rem]">
-            <div className="py-5">
-                <h3 className="text-xl font-semibold">YOUR BALANCE</h3>
-                <h1 className="text-4xl">${Number(expenseList?.reduce((acc, item)=>acc+item.amount,0)).toFixed(2)}</h1>
-            </div>
-            <div className="grid grid-cols-2 bg-white shadow-lg">
-                <span className="flex flex-col justify-center items-center border-r-2">
-                    <h1 className="text-lg font-semibold">INCOME</h1>
-                    <h1 className="text-xl text-green-600">{Number(expenseList?.filter((item)=>item.amount>0).reduce((acc,item)=>acc+item.amount,0)).toFixed(2)}</h1>
-                </span>
-                <span className="flex flex-col justify-center items-center py-3">
-                    <h1 className="text-lg font-semibold">EXPENSE</h1>
-                    <h1 className="text-xl text-red-600">{Number(expenseList?.filter((item)=>item.amount<0).reduce((acc,item)=>acc+item.amount,0)).toFixed(2)}</h1>
-                </span>
-            </div>
-            <div className="py-5">
-                <h1 className="mb-5">Transaction History</h1>
-                <hr className="border-blue-600"/>
-            </div>
+          <div className="py-5">
+            <h3 className="text-xl font-semibold">YOUR BALANCE</h3>
+            <h1 className="text-4xl">
+              $
+              {netGain-netLoss}
+            </h1>
+          </div>
+          <div className="grid grid-cols-2 bg-white shadow-lg">
+            <span className="flex flex-col justify-center items-center border-r-2">
+              <h1 className="text-lg font-semibold">INCOME</h1>
+              <h1 className="text-xl text-green-600">
+                $
+                {netGain}
+              </h1>
+            </span>
+            <span className="flex flex-col justify-center items-center py-3">
+              <h1 className="text-lg font-semibold">EXPENSE</h1>
+              <h1 className="text-xl text-red-600">
+                $
+                {netLoss}
+              </h1>
+            </span>
+          </div>
+          <div className="py-5">
+            <h1 className="mb-5">Transaction History</h1>
+            <hr className="border-blue-600" />
+          </div>
           {expenseList.map((item) => (
-            <List item={item} key={item.id} />
+            <List item={item} key={item.id} assetList={assetList} />
           ))}
         </div>
       </div>
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,13 +1,19 @@
 /* eslint-disable react/prop-types */
 
-const List = ({item}) => {
+const List = ({ item, assetList }) => {
   return (
-    <div className={`mb-3 bg-white p-2 w-full flex gap-x-10 flex-wrap justify-between rounded-lg shadow-lg border-r-4 ${item.amount<0?'border-red-600':'border-green-600'}`}>
-        <span className=''>{item.category}</span>
-        <span className=''>{item.description}</span>
-        <span className=''>${item.amount}</span>
+    <div key={item.id}
+      className={`mb-3 bg-white p-2 w-full grid grid-cols-3 gap-x-10 place-items-center rounded-lg shadow-lg border-r-8 ${
+        assetList.some((asset) => asset === item.category)
+          ? "border-green-600"
+          : "border-red-600"
+      }`}
+    >
+      <span className="">{item.category}</span>
+      <span className="">{item.description}</span>
+      <span className="">${item.amount}</span>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
